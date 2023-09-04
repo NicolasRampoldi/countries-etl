93c34b0e18f26636ce2d90252a62ef6fbe01f64e
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2023-09-03 21:19:40 -0300

    feat: add get countries method

--- persistence/countrydao.py
+++ persistence/countrydao.py
@@ -7,10 +7,21 @@ def create_countries(countries):
     try:
         for country in countries:
             country = Country(**country)
-            session.add(country)
+            # Used merge to avoid errors when the country already exists
+            session.merge(country)
         session.commit()
     except Exception as e:
         session.rollback()
+        raise e
+    finally:
+        session.close()
+
+def get_countries():
+    session = Session()
+    try:
+        countries = session.query(Country).all()
+        return countries
+    except Exception as e:
         raise e
     finally:
         session.close()
