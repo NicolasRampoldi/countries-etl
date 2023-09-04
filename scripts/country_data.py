4d0ddee20020ae7cbdb5cbce15d262e416200550
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2023-09-04 10:04:58 -0300

    feat: add comment

--- scripts/country_data.py
+++ scripts/country_data.py
@@ -13,6 +13,7 @@ def get_country_data():
                 "capital": country_data["capital"][0] if "capital" in country_data else None,
                 "currency": list(country_data["currencies"].values())[0]["name"] if "currencies" in country_data else None,
                 "continent": country_data["region"],
+                # Grab just the first language of the list, in the future we can grab all languages
                 "language": list(country_data["languages"].values())[0] if "languages" in country_data else None,
                 "population": country_data["population"],
                 "flag": country_data["flags"]["png"] if "flags" in country_data else None,
